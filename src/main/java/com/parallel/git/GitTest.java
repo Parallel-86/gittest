52079d9075a1d27283fe9fb8e3eb2cc911eaa4bd
Parallel <zsx_parallel@163.com>
2021-07-13 20:27:14 +0800

    first commit

---
+++ src/main/java/com/parallel/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.parallel.git;
+
+/**
+ * @author parallel
+ * @create 2021-07-13-20:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
